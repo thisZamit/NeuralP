c0d60aedaabf63924fb9a65ddaf9a21c0a254c62
sname <some@gmail.com>
2023-03-03 06:46:55 +0530

    simple flask begining

--- virtenv.py
+++ virtenv.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
-
+from flask import Flask
 print('hellow')
 
 
@@ -10,4 +10,13 @@ except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", 'pandas'])
 finally:
     import pandas as pd
-    
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hey amit"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
