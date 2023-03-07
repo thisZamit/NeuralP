06f773c6d005461301efb92d95ba21dd2b56cf44
amit <thiszamit@gmail.com>
2023-03-07 06:43:35 +0530

    adding setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n",'') for req in requirements]
+    if "-e ." in requirements:
+        requirements.remove("-e .")
+
+setup(
+name='miniProject',
+    version='0.0.1',
+    author= 'Amit',
+    author_email = 'thiszamit@gmail.com',
+    packages= find_packages(),
+    install_requires= get_requirements('requirements.txt')
+)
